0ba631118ecba041043a6da8da46777e5e6fa073
Marian Zburlea <marian@wowdop.com>
2024-12-25 03:28:17 +0400

    add edge list

--- count.ts
+++ count.ts
@@ -1,7 +1,8 @@
 import { cssPropertyList as chromeList } from './util/all-css-properties-in-chrome'
 import { cssPropertyList as chromeCanaryList } from './util/all-css-properties-in-chrome-canary'
+import { cssPropertyList as edgeList } from './util/all-css-properties-in-edge'
 
-const browserMap = { chromeList, chromeCanaryList }
+const browserMap = { chromeList, chromeCanaryList, edgeList }
 const browserList = Object.keys(browserMap)
 
 // count all
