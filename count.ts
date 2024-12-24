f3774f5a885e9255ddc5770c31457722cd9e79de
Marian Zburlea <marian@wowdop.com>
2024-12-25 03:36:48 +0400

    add opera list and count

--- count.ts
+++ count.ts
@@ -2,8 +2,15 @@ import { cssPropertyList as chromeList } from './util/all-css-properties-in-chro
 import { cssPropertyList as chromeCanaryList } from './util/all-css-properties-in-chrome-canary'
 import { cssPropertyList as edgeList } from './util/all-css-properties-in-edge'
 import { cssPropertyList as firefoxList } from './util/all-css-properties-in-firefox'
+import { cssPropertyList as operaList } from './util/all-css-properties-in-opera'
 
-const browserMap = { chromeList, chromeCanaryList, edgeList, firefoxList }
+const browserMap = {
+  chromeList,
+  chromeCanaryList,
+  edgeList,
+  firefoxList,
+  operaList,
+}
 const browserList = Object.keys(browserMap)
 
 // count all
